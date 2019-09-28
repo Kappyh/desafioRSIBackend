c69f587a24cb47e9ee70c3c000df13e297e21d99
Rodolpho <rodolpho@Rodolphos-MacBook-Pro.local>
2019-09-28 13:56:25 -0300

    auth-4

--- src/main/java/br/com/rsifint/controller/AuthController.java
+++ src/main/java/br/com/rsifint/controller/AuthController.java
@@ -3,10 +3,12 @@ package br.com.rsifint.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.rsifint.models.Usuario;
 import br.com.rsifint.services.AuthService;
 
 @RestController
@@ -20,13 +22,17 @@ public class AuthController {
 		this.service = service;
 	}
 
-	@GetMapping("")
-	public ResponseEntity<?> teste() {
-
+	@PostMapping("")
+	public ResponseEntity<?> login(@RequestBody Usuario usuario) {
+		
 		try {
-			return ResponseEntity.status(HttpStatus.OK).body(service.autenticarUsuario("1", "1"));
+			return ResponseEntity
+					.status(HttpStatus.OK)
+					.body(service.autenticarUsuario(usuario));
 		} catch (Exception err) {
-			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Teste");
+			return ResponseEntity
+					.status(HttpStatus.INTERNAL_SERVER_ERROR)
+					.body("Teste");
 		}
 
 	}
