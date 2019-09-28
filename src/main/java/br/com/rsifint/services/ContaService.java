a21e63555a1a8284ba80c99ff581c0b52db37428
Gabriela Mendes <contato@gabrielamendes.dev>
2019-09-28 17:04:14 -0300

    Extrato Controller

--- src/main/java/br/com/rsifint/services/ContaService.java
+++ src/main/java/br/com/rsifint/services/ContaService.java
@@ -3,23 +3,27 @@ package br.com.rsifint.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import br.com.rsifint.models.Conta;
+import br.com.rsifint.dao.ContaDAO;
 import br.com.rsifint.repositories.ContaRepository;
 
 @Service
 public class ContaService {
 
-	
 	private ContaRepository repository;
 
 	@Autowired
 	public ContaService(ContaRepository repository) {
 		this.repository = repository;
 	}
-	
-	public Conta createConta() {
-		Conta conta = new Conta();
-		return conta;
+
+	public String createConta() {
+		try {
+			ContaDAO contaDAO = new ContaDAO();
+			repository.save(contaDAO);
+			return "Conta criada com sucesso";
+		} catch (Exception e) {
+			return "Erro";
+		}
 	}
 
 }
