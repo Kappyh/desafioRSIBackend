cbd3182766694b327b01889e1012fda4055fb9e6
Rodolpho <rodolpho@Rodolphos-MacBook-Pro.local>
2019-09-28 10:58:53 -0300

    primeiro commit

---
+++ src/main/java/br/com/rsifint/RsifintApplication.java
@@ -0,0 +1,13 @@
+package br.com.rsifint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsifintApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RsifintApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rsifint/RsifintApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.rsifint;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RsifintApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
